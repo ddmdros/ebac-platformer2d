9b7e5d773216f7b2e99e35f595cd1477066bfc26
ddmdros <diogome_deiros@hotmail.com>
2025-07-14 10:41:15 -0300

    Added jump to prototype

--- Assets/Scripts/EBAC/Player/Player.cs
+++ Assets/Scripts/EBAC/Player/Player.cs
@@ -3,22 +3,48 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f,0);
     public float speed;
+    public float forceJump = 2;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)||(Input.GetKey(KeyCode.A)))
+
+        HandleJump();
+        HandleMoviment();
+
+    }
+
+    private void HandleMoviment()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
         {
             // myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.linearVelocity = new Vector2(-speed, myRigidbody.linearVelocityY);
         }
-        
-        if (Input.GetKey(KeyCode.RightArrow)||(Input.GetKey(KeyCode.D)))
+
+        if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.linearVelocity = new Vector2(speed, myRigidbody.linearVelocityY);
+        }
+
+        if (myRigidbody.linearVelocity.x > 0)
+        {
+            myRigidbody.linearVelocity += friction;
+        }
+        else if (myRigidbody.linearVelocity.x < 0)
+        {
+            myRigidbody.linearVelocity -= friction;
 
         }
     }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            myRigidbody.linearVelocity = Vector2.up * forceJump;
+        
+    }
+
 }
