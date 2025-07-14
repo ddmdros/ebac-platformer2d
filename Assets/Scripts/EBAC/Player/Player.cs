dbaa1b20bef4c64d2d0a67b61ff740cac220bd2c
ddmdros <diogome_deiros@hotmail.com>
2025-07-14 11:01:48 -0300

    Added run key

--- Assets/Scripts/EBAC/Player/Player.cs
+++ Assets/Scripts/EBAC/Player/Player.cs
@@ -4,8 +4,13 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public Vector2 friction = new Vector2(.1f,0);
+    
     public float speed;
-    public float forceJump = 2;
+    public float speedRun;
+
+    public float forceJump = 2; // it should be jumpForce
+
+    public float _currentSpeed;
 
     void Update()
     {
@@ -17,16 +22,21 @@ public class Player : MonoBehaviour
 
     private void HandleMoviment()
     {
+        if (Input.GetKey(KeyCode.LeftControl))
+            _currentSpeed = speedRun;
+        else 
+            _currentSpeed = speed;
+        
         if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
         {
             // myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-            myRigidbody.linearVelocity = new Vector2(-speed, myRigidbody.linearVelocityY);
+            myRigidbody.linearVelocity = new Vector2(-_currentSpeed, myRigidbody.linearVelocity.y);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
-            myRigidbody.linearVelocity = new Vector2(speed, myRigidbody.linearVelocityY);
+            myRigidbody.linearVelocity = new Vector2(_currentSpeed, myRigidbody.linearVelocityY);
         }
 
         if (myRigidbody.linearVelocity.x > 0)
